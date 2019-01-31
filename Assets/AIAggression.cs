e770c1d3d4d5657a6e170b2d04bb98b2eabf88d6
Alain Garcia <alaingarcia@ufl.edu>
2019-01-31 16:22:51 -0500

    Added Enemy w/Spawning
    - Made Enemy (EnemyRobotBoy) using RobotBoy and then recoloring it red. Removed script that controlled the object using WASD etc. Added script to make the enemy follow the player.
    
    - Made enemy spawner. Can change frequency of spawning and which enemy can be spawned.
    
    TO DO: Make a way for the enemies to be killed (removed from the scene)

---
+++ Assets/AIAggression.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAggression : MonoBehaviour
+
+{
+    public float speed = 6.0f;
+    private GameObject player;
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, direction, speed * Time.deltaTime);
+    }
+}
