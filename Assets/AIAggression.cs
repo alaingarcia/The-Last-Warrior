ddf425329cb87f62a2838fa677b875a8b2410da3
Alain Garcia <alaingarcia@ufl.edu>
2019-01-31 20:00:48 -0500

    Added health, damage, and ability to attack
    - Enemies now have health
    - Left clicking is now an attack (that has customizable damage, currently instantly kills enemies)

--- Assets/AIAggression.cs
+++ Assets/AIAggression.cs
@@ -6,6 +6,7 @@ public class AIAggression : MonoBehaviour
 
 {
     public float speed = 6.0f;
+    private float health = 10.0f;
     private GameObject player;
     private Vector2 direction;
 
@@ -20,5 +21,14 @@ public class AIAggression : MonoBehaviour
     {
         direction = new Vector2(player.transform.position.x, player.transform.position.y);
         transform.position = Vector2.MoveTowards(transform.position, direction, speed * Time.deltaTime);
+    }
+
+    public void takeDamage(float damage)
+    {
+        health = health - damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Attack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public float attackCooldown;
+    private float timeLeft;
+
+    public Transform attackPos;
+    public LayerMask enemyLayer;
+
+    public float attackRange;
+    public int damage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Collider2D[] enemiesDamaged = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayer);
+                for (int i = 0; i < enemiesDamaged.Length; i++)
+                {
+                    enemiesDamaged[i].GetComponent<AIAggression>().takeDamage(damage);
+                }
+            }
+            timeLeft = attackCooldown;
+        }
+
+        else
+        {
+            timeLeft = timeLeft - Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
