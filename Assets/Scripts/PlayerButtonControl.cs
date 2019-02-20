d9beb6fb6f2c8fd29d1f211da2fa61e65816b963
Alain Garcia <alaingarcia@ufl.edu>
2019-02-20 17:09:25 -0500

    Added basic slowdown ability
    - Default ability decreases speed by half
    - Default key is left shift

--- Assets/Scripts/PlayerButtonControl.cs
+++ Assets/Scripts/PlayerButtonControl.cs
@@ -7,15 +7,21 @@ public class PlayerButtonControl : MonoBehaviour
     private float horizontal;
 
     // default jump button will be space
-    private KeyCode jumpKey = KeyCode.Space;
+    public KeyCode jumpKey = KeyCode.Space;
+
+    public KeyCode slowKey = KeyCode.LeftShift;
 
     // gets the movement component of the gameObject
     private Movement movementScript;
+
+    private SlowdownAbility slowdownScript;
 
     void Start() 
     {
         // sets value to the movement script
         movementScript = gameObject.GetComponent<Movement>();
+
+        slowdownScript = gameObject.GetComponent<SlowdownAbility>();
     }
 
     // Update is called once per frame
@@ -28,6 +34,16 @@ public class PlayerButtonControl : MonoBehaviour
         if (Input.GetKeyDown(jumpKey))
         {
             movementScript.jump();
+        }
+
+        if (Input.GetKeyDown(slowKey))
+        {
+            slowdownScript.slowdown();
+        }
+
+        if (Input.GetKeyUp(slowKey))
+        {
+            slowdownScript.normalSpeed();
         }
     }
 }
---
+++ Assets/Scripts/SlowdownAbility.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowdownAbility : MonoBehaviour
+{
+    // list of enemy game objects that will be slowed
+    private GameObject[] enemies;
+
+    // customizable multiplier for enemy slowdown
+    public float enemySlowdownMultiplier = 0.5f;
+
+    // customizable multiplier for player slowdown
+    public float playerSlowdownMultiplier = 1.0f;
+
+    // Function to slow enemies/players down
+    public void slowdown()
+    {
+        // ENEMY SLOWDOWN
+
+        // get a list of all enemies (found by tag)
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        // iterate through the enemy list
+        // save initialSpeed for that gameObject to be order to revert it later
+        foreach (GameObject enemy in enemies)
+        {
+            enemy.GetComponent<Movement>().speed *= enemySlowdownMultiplier;
+        }
+
+        // PLAYER SLOWDOWN
+        gameObject.GetComponent<Movement>().speed *= playerSlowdownMultiplier;
+    }
+
+    // Function to revert speeds back to normal
+    public void normalSpeed()
+    {
+        // ENEMY NORMAL SPEED
+        foreach(GameObject enemy in enemies)
+        {
+            enemy.GetComponent<Movement>().speed /= enemySlowdownMultiplier;
+        }
+
+        // PLAYER NORMAL SPEED
+        gameObject.GetComponent<Movement>().speed /= playerSlowdownMultiplier;
+    }
+}
