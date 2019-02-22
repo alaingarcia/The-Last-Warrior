8d4a3955b6faca871d68dae9e0554a717d597250
Alain Garcia <alaingarcia@ufl.edu>
2019-02-22 12:06:26 -0500

    Added a cooldown to jumping for the AIFollowPlayer script
    - Enemy was jumping non-stop when on the platform
    - Cooldown of 2 seconds seems nice

--- Assets/Scripts/AIFollowPlayer.cs
+++ Assets/Scripts/AIFollowPlayer.cs
@@ -10,6 +10,10 @@ public class AIFollowPlayer : MonoBehaviour
     private float right = 1;
     private float left = -1;
 
+    // set cooldown for jump to prevent constant jumping
+    public float jumpCooldown = 2;
+    private float currentJumpCooldown;
+
     private Vector3 currentLocation;
     private Vector3 playerLocation;
 
@@ -26,11 +30,19 @@ public class AIFollowPlayer : MonoBehaviour
 
         // Set 'body' equal to the current gameObject's Rigidbody (for physics)
         body = gameObject.GetComponent<Rigidbody>();
+
+        // initialize cooldown
+        currentJumpCooldown = jumpCooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentJumpCooldown < jumpCooldown)
+        {
+            currentJumpCooldown += Time.deltaTime;
+        }
+
         // Initialize positions for the current frame
         currentLocation = transform.position;
         playerLocation = player.position;
@@ -40,10 +52,18 @@ public class AIFollowPlayer : MonoBehaviour
         {
             movementScript.move(right);
         }
+
         // If player is below the AI, jump. 1 is added for unnecesary jumping
         if ((currentLocation.y+1) < playerLocation.y )
         {
-            movementScript.jump();
+            // check if cooldown requirement has been met
+            if (currentJumpCooldown >= jumpCooldown)
+            {
+                movementScript.jump();
+
+                // set current cooldown to zero, can't jump again until the current cooldown is back to jumpCooldown
+                currentJumpCooldown = 0;
+            }
         }
         // If player is to the left of the AI, move to the left
         if (currentLocation.x > playerLocation.x)
