6400c283d1f80bacabed351a1e2169c5126ec592
myran2 <henrytgordon@gmail.com>
2019-03-09 19:15:15 -0500

    Add attack and forward/backward animations

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class MeleeAttack : MonoBehaviour
 {
     Collider WeaponHitBox;
+    Animator animator;
     public float damage;
+    public float attackTimeDuration;
     List<GameObject> targets = new List<GameObject>();
+
+    // true when we are inside our attack animation
+    private bool attacking = false;
+    private float attackTimer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         WeaponHitBox = gameObject.GetComponent<Collider>();
+        Debug.Log("Parent of WeaponHitBox: " + gameObject.transform.parent.name);
+        animator = gameObject.transform.parent.GetComponent<Animator>();
     }
 
     void OnTriggerEnter(Collider other)
     {
-
         if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Player")
         {
             targets.Add(other.gameObject);
@@ -31,20 +38,42 @@ public class MeleeAttack : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        // If the attack animation is playing, deal damage to anything hit by our attack animation
+        if (attacking)
+        {
+            foreach (GameObject target in targets)
+            {
+                if (target)
+                {
+                    // Confirm that our target has health before we try to deal damage to it
+                    if (target.GetComponent<Health>() != null)
+                        target.GetComponent<Health>().TakeDamage(damage);
+                }
+            }
+
+            // Reduce the attack timer and stop attacking 
+            if (attackTimer > 0)
+            {
+                attackTimer -= Time.deltaTime;
+            }
+            else
+            {
+                attacking = false;
+            }
+        }
+
+        animator.SetBool("Attacking", attacking);
+    }
+
     public void Attack()
     {
-        foreach (GameObject target in targets)
+        // Only start attacking if we aren't attacking already
+        if (!attacking)
         {
-            if (target)
-            {
-                // Confirm that our target has health before we try to deal damage to it
-                if (target.GetComponent<Health>() != null)
-                    target.GetComponent<Health>().TakeDamage(damage);
-            }
-            /*else
-            {
-                targets.Remove(target);
-            }*/
+            attacking = true;
+            attackTimer = attackTimeDuration;
         }
     }
 }
