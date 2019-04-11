efc0ab66436c93e95518750163a3e2d3917786ef
Alain Garcia <alaingarcia@ufl.edu>
2019-04-11 15:33:17 -0400

    Added Scene Transition
    - New Level for transition between First and Second called "FirstToSecond"
    - New Transition script that adds a fade to black at the beginning of scenes
    - Can now go from "StartMenu" -> "FirstLevel" -> "FirstToSecond" -> "Second"

---
+++ Assets/Scripts/SceneChanging/LevelChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    public string SceneName;
+
+    public void TransitionTo(string levelName)
+    {
+        StartCoroutine(GameObject.FindObjectOfType<Transition>().FadeAndLoadScene(Transition.FadeDirection.Out,levelName));
+    }
+    public void LoadStartMenu()
+    {
+        TransitionTo("StartMenu");
+    }
+    public void LoadFirstLevel()
+    {
+        TransitionTo("FirstLevel");
+    }
+    public void LoadSecondLevel()
+    {
+        TransitionTo("SecondLevel");
+    }
+
+    public void OnTriggerEnter()
+    {
+        TransitionTo(SceneName);
+    }
+}
