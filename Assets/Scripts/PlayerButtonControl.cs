f1d7db5139b4d23bab8cc9fa3abdc54f8e609d57
Alain Garcia <alaingarcia@ufl.edu>
2019-04-14 16:01:09 -0400

    Updated Controls
    - Added alternative controls
    - Fixed being able to attack while Paused

--- Assets/Scripts/PlayerButtonControl.cs
+++ Assets/Scripts/PlayerButtonControl.cs
@@ -9,8 +9,14 @@ public class PlayerButtonControl : MonoBehaviour
 
     // default buttons
     public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode slowKey = KeyCode.LeftShift;
+    public KeyCode jumpKeyAlt = KeyCode.W;
+
+    public KeyCode slowKey = KeyCode.RightShift;
+    public KeyCode slowKeyAlt = KeyCode.Mouse1;
+
     public KeyCode attackKey = KeyCode.Mouse0;
+    public KeyCode attackKeyAlt = KeyCode.LeftShift;
+
     public KeyCode pauseKey = KeyCode.Escape;
 
     // gets the components of the gameObject to call functions
@@ -39,12 +45,12 @@ public class PlayerButtonControl : MonoBehaviour
         movementScript.move(horizontal, vertical);
 
         // if jump button is pressed, call jump from the movement script
-        if (Input.GetKeyDown(jumpKey))
+        if ((Input.GetKeyDown(jumpKey) || Input.GetKeyDown(jumpKeyAlt)) && Time.timeScale > 0f)
         {
             movementScript.jump();
         }
         
-        if (slowdownScript.slowCooldownCurrent > 1)
+        if (slowdownScript.slowCooldownCurrent > 1 && && Time.timeScale > 0f)
         {
             if (Input.GetKeyDown(slowKey))
             {
@@ -52,10 +58,10 @@ public class PlayerButtonControl : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyUp(slowKey) || slowdownScript.slowCooldownCurrent < 1)
+        if ((Input.GetKeyUp(slowKey) || Input.GetKeyUp(slowKeyAlt))|| slowdownScript.slowCooldownCurrent < 1)
             slowdownScript.normalSpeed();
 
-        if (Input.GetKeyDown(attackKey))
+        if ((Input.GetKeyDown(attackKey) || Input.GetKeyDown(attackKeyAlt)) && Time.timeScale > 0f)
             attackScript.Attack();
 
         if (Input.GetKeyDown(pauseKey))
