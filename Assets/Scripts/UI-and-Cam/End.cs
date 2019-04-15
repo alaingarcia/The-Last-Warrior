b373b0611645348ad5072a30efeaadc4c7432b9b
Alain Garcia <alaingarcia@ufl.edu>
2019-04-15 18:28:30 -0400

    Removed team splash to fix end
    - Team splash was causing problems with the displaying of the final screen

--- Assets/Scripts/UI-and-Cam/End.cs
+++ Assets/Scripts/UI-and-Cam/End.cs
@@ -5,19 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour
 {
-    public GameObject image1;
-    public GameObject image2;
     GameObject video;
     bool played;
-    bool splash;
 
     // Start is called before the first frame update
     void Start()
     {
         video = GameObject.FindWithTag("Video");
         video.SetActive(false);
-        image2.SetActive(false);
-        splash = false;
         played = false;
     }
 
@@ -29,14 +24,7 @@ public class End : MonoBehaviour
             SceneManager.LoadScene("StartMenu");
         }
         
-        else if (Input.anyKeyDown && !splash)
-        {
-            image1.SetActive(false);
-            image2.SetActive(true);
-            splash = true;
-        }
-        
-        else if (Input.anyKeyDown && splash)
+        else if (Input.anyKeyDown)
         {
             Invoke("PlayVideo", 2f);
         }
