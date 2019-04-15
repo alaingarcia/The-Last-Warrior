db7bfa5e544aff7f97501354405d81562c8b8414
Alain Garcia <alaingarcia@ufl.edu>
2019-04-15 13:38:38 -0400

    Added End Scene Splash

--- Assets/Scripts/UI-and-Cam/End.cs
+++ Assets/Scripts/UI-and-Cam/End.cs
@@ -5,14 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour
 {
+    public GameObject image1;
+    public GameObject image2;
     GameObject video;
     bool played;
+    bool splash;
 
     // Start is called before the first frame update
     void Start()
     {
         video = GameObject.FindWithTag("Video");
         video.SetActive(false);
+        image2.SetActive(false);
+        splash = false;
         played = false;
     }
 
@@ -24,7 +29,14 @@ public class End : MonoBehaviour
             SceneManager.LoadScene("StartMenu");
         }
         
-        else if (Input.anyKeyDown)
+        else if (Input.anyKeyDown && !splash)
+        {
+            image1.SetActive(false);
+            image2.SetActive(true);
+            splash = true;
+        }
+        
+        else if (Input.anyKeyDown && splash)
         {
             Invoke("PlayVideo", 2f);
         }
